0d1c549261cb4a7b0b9b8f7b8dd540cd6da4bb66
nishan1313 <39843207+nishan1313@users.noreply.github.com>
2018-07-09 18:16:46 -0400

    employee activity

--- app/src/main/java/com/example/developers/myapplication/Login_Activity.java
+++ app/src/main/java/com/example/developers/myapplication/Login_Activity.java
@@ -1,13 +1,26 @@
 package com.example.developers.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Activity extends AppCompatActivity {
+    Button btnSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+        btnSignin =(Button)findViewById(R.id.btnSignin);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login_Activity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/developers/myapplication/MainActivity.java
+++ app/src/main/java/com/example/developers/myapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.developers.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtEmpName,edtBirthDate,edtHourRate,edtWorkedHours,edtintern,edtempsl,edtempbns;
+    CheckBox chkveh,chkCommission,edtFixedAmount;
+    LinearLayout llvehtype,linearpt,linearintern,linearft;
+    RadioGroup rgveh,rgtime;
+    RadioButton rbcar,rbmotorcycle,rbIntern,rbFullTime;
+    Button btnSavePayRoll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmpName =(EditText)findViewById(R.id.edtEmpName);
+        edtEmpName =(EditText)findViewById(R.id.edtEmpName);
+        edtEmpName =(EditText)findViewById(R.id.edtEmpName);
+        edtEmpName =(EditText)findViewById(R.id.edtEmpName);
+        
     }
 }
